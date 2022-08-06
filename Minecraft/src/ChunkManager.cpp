065d8a47d73d82fbd2d663ed272978203db8e776
nlins8224 <nlins8224@gmail.com>
2022-08-06 15:36:31 +0200

    Move current_chunk to heap

--- Minecraft/src/ChunkManager.cpp
+++ Minecraft/src/ChunkManager.cpp
@@ -16,22 +16,22 @@ void ChunkManager::generateWorld()
 		for (int j = 0; j < 2; j++)
 		{
 			chunk_pos chunk_position(i - 4, -1, j - 4);
-			Chunk current_chunk(&m_texture_manager, chunk_position);
-			for (int x = 0; x < current_chunk.CHUNK_SIZE; x++)
+			std::unique_ptr<Chunk> current_chunk(new Chunk (&m_texture_manager, chunk_position));
+			for (int x = 0; x < current_chunk->CHUNK_SIZE; x++)
 			{
-				for (int y = 0; y < current_chunk.CHUNK_SIZE; y++)
+				for (int y = 0; y < current_chunk->CHUNK_SIZE; y++)
 				{
-					for (int z = 0; z < current_chunk.CHUNK_SIZE; z++)
+					for (int z = 0; z < current_chunk->CHUNK_SIZE; z++)
 					{
 						if (z % 2)
-							current_chunk.setBlock(x, y, z, Block::block_id::SAND);
+							current_chunk->setBlock(x, y, z, Block::block_id::SAND);
 						else
-							current_chunk.setBlock(x, y, z, Block::block_id::COBBLESTONE);
+							current_chunk->setBlock(x, y, z, Block::block_id::COBBLESTONE);
 					}	
 				}
 			}
 
-			m_chunks[chunk_position] = current_chunk;
+			m_chunks[chunk_position] = *current_chunk;
 			
 		}
 	}
