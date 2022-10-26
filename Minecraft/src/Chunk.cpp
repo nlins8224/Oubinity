29bfeb592ede0cc5f85a360941a13d6a1c4d0965
nlins8224 <nlins8224@gmail.com>
2022-10-26 20:51:45 +0200

    Refactor int xyz to vectors

--- Minecraft/src/Chunk.cpp
+++ Minecraft/src/Chunk.cpp
@@ -40,9 +40,9 @@ void Chunk::prepareChunkMesh()
 		{
 			for (uint8_t local_z = 0; local_z < CHUNK_SIZE; local_z++)
 			{
-				block = getBlockId(local_x, local_y, local_z);
+				block = getBlockId(glm::ivec3(local_x, local_y, local_z));
 				if (block != block_id::AIR)
-					addVisibleFaces(local_x, local_y, local_z);
+					addVisibleFaces(glm::ivec3(local_x, local_y, local_z));
 			}
 		}
 	}
@@ -63,33 +63,36 @@ void Chunk::renderChunk()
 	glDrawArrays(GL_TRIANGLES, 0, amount_of_triangles);
 }
 
-void Chunk::setBlock(uint8_t x, uint8_t y, uint8_t z, block_id type)
+void Chunk::setBlock(glm::ivec3 pos, block_id type)
 {
-	m_blocks[x][y][z] = type;
+	m_blocks[pos.x][pos.y][pos.z] = type;
 }
 
-void Chunk::addVisibleFaces(uint8_t x, uint8_t y, uint8_t z)
+void Chunk::addVisibleFaces(glm::ivec3 pos)
 {
-	if (!isFaceVisible(x + 1, y, z)) addFace(faces[block_mesh::RIGHT],  x, y, z);
-	if (!isFaceVisible(x - 1, y, z)) addFace(faces[block_mesh::LEFT],   x, y, z);
-	if (!isFaceVisible(x, y + 1, z)) addFace(faces[block_mesh::TOP],    x, y, z);
-	if (!isFaceVisible(x, y - 1, z)) addFace(faces[block_mesh::BOTTOM], x, y, z);
-	if (!isFaceVisible(x, y, z + 1)) addFace(faces[block_mesh::FRONT],  x, y, z);
-	if (!isFaceVisible(x, y, z - 1)) addFace(faces[block_mesh::BACK],   x, y, z);
+	int x = pos.x, y = pos.y, z = pos.z;
+
+	if (!isFaceVisible(glm::ivec3(x + 1, y, z))) addFace(faces[block_mesh::RIGHT],  glm::ivec3(x, y, z));
+	if (!isFaceVisible(glm::ivec3(x - 1, y, z))) addFace(faces[block_mesh::LEFT],   glm::ivec3(x, y, z));
+	if (!isFaceVisible(glm::ivec3(x, y + 1, z))) addFace(faces[block_mesh::TOP],	glm::ivec3(x, y, z));
+	if (!isFaceVisible(glm::ivec3(x, y - 1, z))) addFace(faces[block_mesh::BOTTOM], glm::ivec3(x, y, z));
+	if (!isFaceVisible(glm::ivec3(x, y, z + 1))) addFace(faces[block_mesh::FRONT],	glm::ivec3(x, y, z));
+	if (!isFaceVisible(glm::ivec3(x, y, z - 1))) addFace(faces[block_mesh::BACK],	glm::ivec3(x, y, z));
 }
 
-bool Chunk::isFaceVisible(int8_t x, int8_t y, int8_t z)
+bool Chunk::isFaceVisible(glm::ivec3 pos)
 {
+	int x = pos.x, y = pos.y, z = pos.z;
 	// out of bounds check for example...
 	if (x < 0 || y < 0 || z < 0) return false; // ...x - 1 = -1 < 0
 	if (x >= CHUNK_SIZE || y >= CHUNK_SIZE || z >= CHUNK_SIZE) return false; // ...x + 1 = 16 > 15
 	return m_blocks[x][y][z] != block_id::AIR;
 }
 
-void Chunk::addFace(std::array<float, FACE_SIZE> const &face, int x, int y, int z)
+void Chunk::addFace(std::array<float, FACE_SIZE> const &face, glm::ivec3 pos)
 {
 	const uint8_t FACE_ROWS{ 6 };
-	int block_id{ getBlockId(x, y, z) };
+	int block_id{ getBlockId(pos) };
 	int texture_id{ setFaceTexture(block_id) };
 
 	uint8_t x_coord;
@@ -110,9 +113,9 @@ void Chunk::addFace(std::array<float, FACE_SIZE> const &face, int x, int y, int 
 		u_coord = (i * 6) + 3;
 		v_coord = (i * 6) + 4;
 		
-		x_world_pos = m_chunk_position.x * CHUNK_SIZE + x + face[x_coord];
-		y_world_pos = m_chunk_position.y * CHUNK_SIZE + y + face[y_coord];
-		z_world_pos = m_chunk_position.z * CHUNK_SIZE + z + face[z_coord];
+		x_world_pos = m_chunk_position.x * CHUNK_SIZE + pos.x + face[x_coord];
+		y_world_pos = m_chunk_position.y * CHUNK_SIZE + pos.y + face[y_coord];
+		z_world_pos = m_chunk_position.z * CHUNK_SIZE + pos.z + face[z_coord];
 
 		m_mesh_vertex_positions.emplace_back(x_world_pos);
 		m_mesh_vertex_positions.emplace_back(y_world_pos);
@@ -132,7 +135,7 @@ int Chunk::setFaceTexture(int8_t block_id)
 
 // This could be optimised. For now chunk cannot recognize if a face is not visible,
 // because other neighboring chunk hides it.
-int Chunk::getBlockId(uint8_t x, uint8_t y, uint8_t z)
+int Chunk::getBlockId(glm::ivec3 pos)
 {
-	return m_blocks[x][y][z];
+	return m_blocks[pos.x][pos.y][pos.z];
 }
