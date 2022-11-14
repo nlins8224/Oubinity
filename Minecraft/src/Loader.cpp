6a6aa95d8fe14dcb9bfbc7b0b868809b384c8c99
nlins8224 <nlins8224@gmail.com>
2022-11-14 21:43:31 +0100

    WIP: Terrain generation, add lighting

--- Minecraft/src/Loader.cpp
+++ Minecraft/src/Loader.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 
-void Loader::loadMesh(std::vector<float>& mesh_vertices, std::vector<float>& mesh_textures)
+void Loader::loadMesh(std::vector<float>& mesh_vertices, std::vector<float>& mesh_textures, std::vector<float>& mesh_shades)
 {
 	createVAO();
-	storeDataInVAO(0, mesh_vertices, mesh_textures);
+	storeDataInVAO(0, mesh_vertices, mesh_textures, mesh_shades);
 	unbindVAO();
 }
 
@@ -14,7 +14,7 @@ void Loader::createVAO() {
 	glBindVertexArray(m_vao);
 }
 
-void Loader::storeDataInVAO(int attribute_number, std::vector<float>& mesh_vertices, std::vector<float>& mesh_textures) {
+void Loader::storeDataInVAO(int attribute_number, std::vector<float>& mesh_vertices, std::vector<float>& mesh_textures, std::vector<float>& mesh_shades) {
 	glGenBuffers(1, &m_vertex_vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertex_vbo);
 	glBufferData(GL_ARRAY_BUFFER, mesh_vertices.size() * sizeof(float), mesh_vertices.data(), GL_STATIC_DRAW);
@@ -29,6 +29,13 @@ void Loader::storeDataInVAO(int attribute_number, std::vector<float>& mesh_verti
 	// store uvw textures coords
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
+
+	//store shading values
+	glGenBuffers(1, &m_shading_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_shading_vbo);
+	glBufferData(GL_ARRAY_BUFFER, mesh_shades.size() * sizeof(float), mesh_shades.data(), GL_STATIC_DRAW);
+	glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, sizeof(float), (void*)0);
+	glEnableVertexAttribArray(2);
 }
 
 void Loader::bindVAO() {
---
+++ Minecraft/src/WorldGenerator.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Chunk.h"
+#include "FastNoiseLite.h"
+#include <iostream>
+
+using height_map = std::array<std::array<uint8_t, Chunk::CHUNK_SIZE_X>, Chunk::CHUNK_SIZE_Z>;
+
+class WorldGenerator
+{
+public:
+	height_map generateChunkHeightMap(glm::ivec3 chunk_pos, const int world_seed) const;
+};
+
