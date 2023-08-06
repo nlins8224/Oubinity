6be6f5f5b88540b117997b29f31edf87f8a20f81
nlins8224 <nlins8224@gmail.com>
2023-08-06 18:07:31 +0200

    Remove: Ray, DecorationGenerator, NoiseMapEffect, Spline, SplineControlPoints permamently

--- Minecraft/src/terrain_generation/TerrainGenerator.h
+++ Minecraft/src/terrain_generation/TerrainGenerator.h
@@ -2,12 +2,8 @@
 #include "../chunk/Chunk.h"
 #include "../chunk/ChunkSize.h"
 #include "shapes/NoiseGenerator.h"
-#include "shapes/Spline.h"
-#include "shapes/DecorationGenerator.h"
 #include "BiomeGenerator.h";
-#include "ShapeGenerator.h"
-//#include "../tree/Tree.h"
-
+#include "ShapeGenerator.h";
 
 class TerrainGenerator
 {
@@ -15,7 +11,6 @@ public:
 	TerrainGenerator(int world_seed, uint8_t surface_height, uint8_t water_height);
 	~TerrainGenerator() = default;
 	void generateChunkTerrain(Chunk& chunk);
-	//void decorateChunkTerrain(Chunk& chunk);
 	ShapeGenerator& getShapeGenerator();
 	float getSurfaceHeight(glm::ivec2 chunk_pos_xz, glm::ivec2 block_pos_xz);
 private:
