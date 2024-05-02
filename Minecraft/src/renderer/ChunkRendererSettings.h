4d06ba5f8348711c8aa26c8394ef79608fdde776
nlins8224 <nlins8224@gmail.com>
2024-05-02 18:43:20 +0200

    Add texture pack and assets

--- Minecraft/src/renderer/ChunkRendererSettings.h
+++ Minecraft/src/renderer/ChunkRendererSettings.h
@@ -2,6 +2,6 @@
 
 namespace ChunkRendererSettings
 {
-	constexpr int MAX_RENDERED_CHUNKS_IN_XZ_AXIS = 64;
+	constexpr int MAX_RENDERED_CHUNKS_IN_XZ_AXIS = 128;
 	constexpr int MAX_RENDERED_CHUNKS_IN_Y_AXIS = 32;
 }
